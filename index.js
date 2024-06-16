29af94bfeb0896e851d6fbb6ba41951308a45f94
teshager <teshager8922@gmail.com>
2024-06-16 09:51:39 +0300

    add globla variables to keep score

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("Hello World!");
 
+let humanScore=0,computerScore=0;
+
 const getComputerChoice=()=>{
 Math.floor(Math.random()*3);
 
@@ -8,6 +10,10 @@ Math.floor(Math.random()*3);
 const getHumanChoice=()=>{
     return choice=prompt("Input:0 for paper\n      1 for Scissor\n      2 for Rock\nOne, Two, three go:");
     
+}
+
+const playRound=()=>{
+
 }
 
 getHumanChoice();
