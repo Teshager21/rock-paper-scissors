088431abcbfd381fbf3ea76d917ac0be284f930c
teshager <teshager8922@gmail.com>
2024-06-16 09:48:22 +0300

    write a function that prompts the user for input and returns the choice

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 console.log("Hello World!");
 
 const getComputerChoice=()=>{
-Math.floor(Math.random()*4);
+Math.floor(Math.random()*3);
 
 }
 
+const getHumanChoice=()=>{
+    return choice=prompt("Input:0 for paper\n      1 for Scissor\n      2 for Rock\nOne, Two, three go:");
+    
+}
 
+getHumanChoice();
 getComputerChoice();
