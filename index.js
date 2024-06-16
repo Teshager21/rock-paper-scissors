752655fe339c001bcb7acce9cf14aea53a7a8f56
teshager <teshager8922@gmail.com>
2024-06-16 17:27:34 +0300

    style the results card

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 console.log("Hello Welcome to this game!");
 
 let humanScore=0,computerScore=0;
+const container= document.querySelector('.container');
+let result=document.createElement('div');
+result.classList.add('card');
 const choices=['paper','scissor','rock'];
 const getComputerChoice=()=>{
 return Math.floor(Math.random()*3);
+
+
 
 }
 
@@ -22,12 +27,13 @@ const getHumanChoice=()=>{
 }
 
 const playGame=()=>{
+      //DOM MANIPULATION
+     
+      
     for(i=0;i<5;i++){
         playRound(getHumanChoice(),getComputerChoice());
     }
-    //DOM MANIPULATION
-    const result=document.createElement('div');
-    const container= document.querySelector('.container');
+  
     if(humanScore===computerScore){
         console.log("Its a tie");
         result.textContent="Its a tie";
@@ -35,13 +41,16 @@ const playGame=()=>{
 
     }else if(humanScore>computerScore){
         console.log('You Won the Game!!!',`${humanScore} to ${computerScore}`)
-        result.textContent='You Won the Game!!!',`${humanScore} to ${computerScore}`;
+        result.textContent('You Won the Game!!!',`${humanScore} to ${computerScore}`);
     }else {
-        console.log("OOPs, You Lost",`${humanScore} to ${computerScore}`);
+        console.log("Oops, You Lost",`${humanScore} to ${computerScore}`);
         result.textContent="OOPs, You Lost",`${humanScore} to ${computerScore}`;
+        
     }
 
+   
     container.appendChild(result);
+  
     
 }
 
@@ -54,6 +63,7 @@ const playRound=(humanChoice,computerChoice)=>{
     //WHEN THERE IS A TIE
     if(human===computer){
         console.log("This round  a tie");
+        result.textContent="This round  a tie";
     }
     if(diff===1) winner = Math.max(human,computer);
     if(diff==2) winner=Math.min(human,computer);
@@ -61,13 +71,15 @@ const playRound=(humanChoice,computerChoice)=>{
     if (winner===human){
         humanScore++;
         console.log("Dude, You Won this round! ",`${choices[human]} beats ${choices[computer]}`)
+        result.textContent=(`Dude, You Won this round! ${choices[human]} beats ${choices[computer]}`)
     } 
     if(winner===computer) {
-        console.log("Dude, You Lost this round! ",`${choices[computer]} beats ${choices[human]}`)
+        console.log(`Dude, You Lost this round! ${choices[computer]} beats ${choices[human]}`)
+        result.textContent=(`Dude, You Lost this round! ${choices[computer]} beats ${choices[human]}`)
         computerScore++;
      
     }
-     
+    container.appendChild(result);   
 
 }
 
