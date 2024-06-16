58aae8d6ac239cb203cddd4cae338f301976717d
teshager <teshager8922@gmail.com>
2024-06-16 10:28:02 +0300

    write pseudocode for the game rule

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 console.log("Hello World!");
 
 let humanScore=0,computerScore=0;
-
+const choices=['paper','scissor','rock'];
 const getComputerChoice=()=>{
-Math.floor(Math.random()*3);
+return Math.floor(Math.random()*3);
 
 }
 
 const getHumanChoice=()=>{
+
     choice=prompt("Type one of the options below:\n -Paper\n -Scissor or  \n -Rock,\n One Two, three go:").trim().toLocaleLowerCase();
-    console.log(choice);
+    // console.log(choice);
+    return choices.indexOf(choice);
 }
 
+const playRound=(humanChoice,computerChoice)=>{
+    // [1,1]= if they are equal ====>it is a tie
+    // [0,1],[1,2]==>computerwon----diff of -1
+    // [1,0],[1,2]=>humanwon---------diff of +1
+    //----when the abs(diff)===1 , the greater wins
+    // [0,2]=>humanwon=-------------diff of -2
+    // [2,0]=>computerwon------------diff +2
+    //-----when the abs(diff)===2, the lower wins
+    
 
+    console.log(`index of paper is : ${choices.indexOf('scissor')}`);
+    console.log(humanChoice ,computerChoice);    
+     
+
+}
+playRound(getHumanChoice(),getComputerChoice());
 
 getHumanChoice();
 getComputerChoice();
